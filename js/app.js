63029946bfe34d1d2e74ceeeb7df41b9012044ed
Luis Francisco <183943582+luisfrancisco2b@users.noreply.github.com.>
2026-09-03 22:09:48 -0300

    chore: add DOM element selectors for to-do app

---
+++ js/app.js
@@ -0,0 +1,14 @@
+// Elements Selection
+const toDoForm = document.querySelector("#to-do-form");
+const editForm = document.querySelector("#edit-form");
+
+const toDoInput = document.querySelector("#to-do-input");
+const editInput = document.querySelector("#edit-input");
+
+const cancelEditBtn = document.querySelector("#cancel-edit-btn");
+
+const toDoList = document.querySelector("#to-do-list");
+
+// Functions
+
+// Events
